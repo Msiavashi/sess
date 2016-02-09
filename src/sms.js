9232069d960a9c28ace84f10d2e5451bbd66d9a2
ms95 <mohammad.siav@hotmail.com>
2016-02-10 02:35:47 +0330

    fixes in sending sms

--- src/sms.js
+++ src/sms.js
@@ -13,6 +13,8 @@ var {
 } = React;
 
 var selfServices = ['مهندسی نفت و گاز', 'مرکزی', 'ارم', 'خوابگاه شهید دستغیب', 'دانشکده هنر و معماری', 'دانشکده کشاورزی', 'بوفه ارم', 'بوفه مرکزی', 'بوفه خوابگاه مفتح', 'دانشکده دامپزشکی', 'خوابگاه دامپزشکی', 'دانشکده علوم'];
+var selfNumbers = [];
+var commands = [{jetonViaSMS: '#1#'}];    //the commands that are used in sms's should be add to here
 /*
 SmsAndroid.sms(
   '123456789', // phone number to send sms to
@@ -28,16 +30,60 @@ SmsAndroid.sms(
 );
 */
 var SMSPanelView = React.createClass({
+  _selectFieldOnPress(){
+    this.setState({
+
+    });
+  },
   _backButtonPressed(){
     this.props.navigator.push({ id: "IndexView" });
   },
   getInitialState: function() { //they are used for Modal view
     return {
+      /*modal variables*/
       isOpen: false,
       isDisabled: false,
-      swipeToClose: true
+      swipeToClose: true,
+      /*selections variables*/
+      forgottenFirstFood: 'off',
+      forgottenSecondFood: 'off',
+      jetonViaSMS: 'off'
     };
   },
+  /*TODO: find a shortcut for this radio-like buttons (this function is only for test & not yet effecient to use ) */
+  selectionChanged(selected){
+    switch (selected) {
+      case "forgottenFirstFood":
+        this.setState({
+          forgottenFirstFood: 'on',
+          forgottenSecondFood: 'off',
+          jetonViaSMS: 'off',
+        });
+        break;
+      case "forgottenSecondFood":
+        this.setState({
+          forgottenFirstFood: 'off',
+          forgottenSecondFood: 'on',
+          jetonViaSMS: 'off',
+        });
+        break;
+      case "jetonViaSMS":
+        this.setState({
+          forgottenFirstFood: 'off',
+          forgottenSecondFood: 'off',
+          jetonViaSMS: 'on',
+        });
+        break;
+      default:
+        break;
+    }
+  },
+  sendButtonOnPress(){
+    //TODO: write the body
+    //find the number of the self
+    //find what is the selection of the user
+    //call the sendSMS fucntion to perform the rest
+  }
   openmodalView(id){
     this.refs.modalView.open();
   },
@@ -58,12 +104,12 @@ var SMSPanelView = React.createClass({
               <View style = {{flex: 5, backgroundColor: 'lightblue', borderRadius: 5, borderColor: 'white'}}>
 
                 <View style = {{flex: 1, alignItems: 'center', flexDirection: 'row', borderWidth: 1, borderColor: 'grey'}}>
-                  <Button style = {{flex: 1}}>   انتخاب  </Button>
+                  <Button style = {{flex: 1}} onPress = {() => this.selectionChanged("forgottenFirstFood")}>   {this.state.forgottenFirstFood}  </Button>
                   <Text style = {{flex: 1, fontSize: 18, fontWeight: 'bold'}}> غذای یک </Text>
                 </View>
 
                 <View style = {{flex: 1, alignItems: 'center', flexDirection: 'row', borderWidth: 1, borderColor: 'grey'}}>
-                  <Button style = {{flex: 1}}>   انتخاب  </Button>
+                  <Button style = {{flex: 1}} onPress = {() => this.selectionChanged("forgottenSecondFood")}>   {this.state.forgottenSecondFood}  </Button>
                   <Text style = {{flex: 1, fontSize: 18, fontWeight: 'bold'}}> غذای دو </Text>
                 </View>
 
@@ -71,13 +117,13 @@ var SMSPanelView = React.createClass({
               <Text> سایر گزینه ها </Text>
               <View style = {{flex: 5, backgroundColor: 'lightblue', borderRadius: 5, borderColor: 'white'}}>
                 <View style = {{flex: 1, alignItems: 'flex-start', flexDirection: 'row', borderWidth: 1, borderColor: 'grey'}}>
-                  <Button style = {{flex: 1}}>   انتخاب  </Button>
+                  <Button style = {{flex: 1}} onPress = {() => this.selectionChanged("jetonViaSMS")}>   {this.state.jetonViaSMS}  </Button>
                   <Text style = {{flex: 1, fontSize: 18, fontWeight: 'bold'}}>  بدون کارت </Text>
                 </View>
 
               </View>
               <View style = {{ justifyContent:'center', flex:1, backgroundColor: '#CDBA96', borderWidth: 2, borderColor: '#EED8AE'}}>
-                  <Button style = {{flex: 1,}}> ارسال </Button>
+                  <Button style = {{flex: 1,}} onPress = {this.sendButtonOnPress}> ارسال </Button>
               </View>
       </Modal>
     </View>
